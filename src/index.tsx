5761243b15a00ae3c6c8c9b5812157a35344eb61
shiauweitsung <zz830527@gmail.com>
2022-12-02 17:13:16 +0800

    [add] add i18n lauguage

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'store';
 import { Provider } from 'react-redux';
+import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,26 @@
 import { useMemo, useState } from 'react';
 import Button from 'components/Button';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { increment, decrement } from 'store/counterSlice';
+import { useAppDispatch } from 'store/hook';
 
 export function Home() {
   const [count, setCount] = useState<number>(0);
+  const dispatch = useAppDispatch();
+
+  const { t, i18n } = useTranslation();
+
   const memo = useMemo(() => {
     console.log(count, 'count');
 
     return count > 5 ? '5' : '----';
   }, [count]);
   console.log('render');
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <div>
@@ -18,6 +29,7 @@ export function Home() {
       <Button
         onClick={() => {
           setCount((prev) => prev + 1);
+          dispatch(increment());
         }}
       >
         increament
@@ -25,9 +37,26 @@ export function Home() {
       <Button
         onClick={() => {
           setCount((prev) => prev - 1);
+          dispatch(decrement());
         }}
       >
         decreament
+      </Button>
+      <h2>i18n 語系</h2>
+      <h2>{t('Welcome to React')}</h2>
+      <Button
+        onClick={() => {
+          changeLanguage('en');
+        }}
+      >
+        切換英文
+      </Button>
+      <Button
+        onClick={() => {
+          changeLanguage('tw');
+        }}
+      >
+        切換中文
       </Button>
     </div>
   );
