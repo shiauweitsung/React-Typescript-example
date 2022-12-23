144ad31f8fb16568e3387600d7cd8c40d6d093d0
shiauweitsung <zz830527@gmail.com>
2022-12-23 17:36:25 +0800

    add bot ai

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const { NlpManager } = require('node-nlp');
+const manager = new NlpManager({ languages: ['en'] });
 const leaveRoom = require('./utils/leave-room');
 
 const corsOptions = {
@@ -28,6 +30,39 @@ const io = new Server(server, {
 const CHAT_BOT = 'ChatBot';
 let chatRoom = ''; // E.g. javascript, node,...
 let allUsers = []; // All users in current chat room
+
+async function trainChatBotIA() {
+    return new Promise(async (resolve, reject) => {
+        // Adds the utterances and intents for the NLP
+        // // Train also the NLG
+        // Adds the utterances and intents for the NLP
+        manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+        manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+        manager.addDocument('en', 'okay see you later', 'greetings.bye');
+        manager.addDocument('en', 'bye for now', 'greetings.bye');
+        manager.addDocument('en', 'i must go', 'greetings.bye');
+        manager.addDocument('en', 'hello', 'greetings.hello');
+        manager.addDocument('en', 'hi', 'greetings.hello');
+        manager.addDocument('en', 'howdy', 'greetings.hello');
+        // Train also the NLG
+        manager.addAnswer('en', 'greetings.bye', 'Till next time');
+        manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+        manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+        manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+        await manager.train();
+        manager.save();
+        console.log("AI has been trainded")
+        resolve(true);
+    })
+}
+trainChatBotIA();
+async function generateResponseAI(qsm) {
+    // Train and save the mode
+    return new Promise(async (resolve, reject) => {
+        response = await manager.process('en', qsm);
+        resolve(response);
+    })
+}
 
 // Listen for when the client connects via socket.io-client
 io.on('connection', (socket) => {
@@ -64,17 +99,20 @@ io.on('connection', (socket) => {
 
     });
     // send message
-    socket.on('message', (data) => {
+    socket.on('message', async (data) => {
         console.log(data, 'message');
-        socket.emit('messageResponse', data)
-        if (data.message === 'test') {
-            console.log('return test');
-            socket.emit('receive_message', {
-                message: `u say test`,
-                name: CHAT_BOT,
-                __createdtime__,
-            });
-        }
+        socket.emit('messageResponse', data);
+        let response = await generateResponseAI(data.message);
+        console.log(response, 'response');
+        socket.emit('receive_message', response.answer !== undefined ? {
+            message: response.answer,
+            name: CHAT_BOT,
+            __createdtime__,
+        } : {
+            message: `I don't understand`,
+            name: CHAT_BOT,
+            __createdtime__,
+        })
     });
 
     // leave room
